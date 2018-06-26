0d96ef270c24fc15501a76c8b1d1ce5580cd1573
Brown <brownr@city.charlottesville.org>
2018-06-26 14:18:13 -0400

    Simplified answer(xs)

--- foobar2.py
+++ foobar2.py
@@ -1,16 +1,24 @@
 array1 = [-200, -34, 0, 1, 2, 10, 1000]
 array2 = [-200, -34, -5, 0, 1, 2, 10, 1000]
 array3 = [2, 0, 2, 2, 0]
-
+array4 = [-3]
+'''
 def answer(xs):
   pos_xs = [x for x in xs if x > 0]
   neg_xs = [x for x in xs if x < 0]
   if len(neg_xs) % 2 != 0:
     neg_xs.remove(max(neg_xs))
   data_list = neg_xs + pos_xs
-  power = reduce(lambda x,y: x*y, data_list)
-  power_str = str(power)
+  power_int = reduce(lambda x,y: x*y, data_list)
+  power_str = str(power_int)
   return power_str
+'''
+def answer(xs):
+  xs = [x for x in xs if x != 0]
+  negs = [x for x in xs if x < 0]
+  if len(negs) % 2 != 0:
+    xs.remove(max(negs))
+  return str(reduce(lambda x,y: x*y, xs))
 
 print (answer(array1))
 print (answer(array2))
