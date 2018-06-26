1470cff24b654ed3698338d283d8778f348b2971
Brown <brownr@city.charlottesville.org>
2018-06-26 14:50:36 -0400

    Works, only failing test 3

--- foobar2.py
+++ foobar2.py
@@ -2,6 +2,8 @@ array1 = [-200, -34, 0, 1, 2, 10, 1000]
 array2 = [-200, -34, -5, 0, 1, 2, 10, 1000]
 array3 = [2, 0, 2, 2, 0]
 array4 = [-3]
+array5 = [6]
+array6 = [2,-3,1,0,-5]
 '''
 def answer(xs):
   pos_xs = [x for x in xs if x > 0]
@@ -16,17 +18,17 @@ def answer(xs):
 def answer(xs):
   while 0 in xs: xs.remove(0)
   negs = [x for x in xs if x < 0]
-  if len(negs) % 2 != 0 and len(negs) != 1:
+  if len(negs) == 1 and len(xs) == 1:
+    power_int = 0
+  elif len(negs) % 2 != 0 and len(negs) != 1:
     xs.remove(max(negs))
-  elif len(negs) % 2 != 0 and len(xs) == 1:
-    power_str = negs[0]
-  else:
-    power_int = reduce(lambda x,y: x*y, xs)
-    power_str = str(power_int)
+  power_int = reduce(lambda x,y: x*y, xs)
+  power_str = str(power_int)
   return power_str
 
 print (answer(array1))
 print (answer(array2))
 print (answer(array3))	
 print (answer(array4))
-
+print (answer(array5))
+print (answer(array6))
