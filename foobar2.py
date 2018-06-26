4e43796385a3fccc4f1e4aee861761e2a4daf1cb
Brown <brownr@city.charlottesville.org>
2018-06-26 15:57:29 -0400

    Made single value negative arrays output negative result, pass test 4, fail test 3

--- foobar2.py
+++ foobar2.py
@@ -4,9 +4,9 @@ def answer(xs):
   #If single negative number given output 0
   negs = [x for x in xs if x < 0]
   if len(negs) == 1 and len(xs) == 1:
-    power_int = 0
+    power_int = negs[0]
   #Address odd number of negative values by removing smallest abs value  
-  if len(negs) % 2 != 0:
+  elif len(negs) % 2 != 0:
     xs.remove(max(negs))
   #If length of xs is 0, output 0 (Causes by array with only 0 values)
   if len(xs) == 0:
@@ -14,11 +14,12 @@ def answer(xs):
   #All other cases multiply xs together to get max output
   else:
     power_int = reduce(lambda x,y: x*y, xs)
-  #Turn to int to str
+  #Turn int to str
   power_str = str(power_int)
   return power_str
 
 def test_func():
+  a1 = [0, 0]
   a2 = [2]
   a3 = [0]
   a4 = [-2]
@@ -27,7 +28,9 @@ def test_func():
   a7 = [-2,-2]
   a8 = [-2,-2,2]
   a9 = [-2,2,2]
-  arrays = [a2, a3, a4, a5, a6, a7, a8, a9]
+  a10 = [-1000,1000,-1000]
+
+  arrays = [a1, a2, a3, a4, a5, a6, a7, a8, a9, a10]
 
   for a in arrays:
     print 'Array: {0}   ||   Output: {1}'.format(a, answer(a))
