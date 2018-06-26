1e8f3e12ae2d7db09958187520f68cea49c03088
Brown <brownr@city.charlottesville.org>
2018-06-26 15:12:53 -0400

    Added tests, failing single neg, multiple pos, and single neg single pos

--- foobar2.py
+++ foobar2.py
@@ -1,34 +1,35 @@
-array1 = [-200, -34, 0, 1, 2, 10, 1000]
-array2 = [-200, -34, -5, 0, 1, 2, 10, 1000]
-array3 = [2, 0, 2, 2, 0]
-array4 = [-3]
-array5 = [6]
-array6 = [2,-3,1,0,-5]
-'''
 def answer(xs):
-  pos_xs = [x for x in xs if x > 0]
-  neg_xs = [x for x in xs if x < 0]
-  if len(neg_xs) % 2 != 0:
-    neg_xs.remove(max(neg_xs))
-  data_list = neg_xs + pos_xs
-  power_int = reduce(lambda x,y: x*y, data_list)
-  power_str = str(power_int)
-  return power_str
-'''
-def answer(xs):
+  #Remove 0 values
   while 0 in xs: xs.remove(0)
+  #Get list of negative values
   negs = [x for x in xs if x < 0]
+  #If length of negs is 1 and length of xs is 1, output 0
   if len(negs) == 1 and len(xs) == 1:
     power_int = 0
   elif len(negs) % 2 != 0 and len(negs) != 1:
     xs.remove(max(negs))
-  power_int = reduce(lambda x,y: x*y, xs)
+  elif len(negs) % 2 != 0 and len(negs) == 1:
+    power_int = 0
+  elif len(xs) == 0:
+    power_int = 0 
+  else:
+    power_int = reduce(lambda x,y: x*y, xs)
   power_str = str(power_int)
   return power_str
 
-print (answer(array1))
-print (answer(array2))
-print (answer(array3))	
-print (answer(array4))
-print (answer(array5))
-print (answer(array6))
+def test_func():
+  a2 = [2]
+  a3 = [0]
+  a4 = [-2]
+  a5 = [2,2]
+  a6 = [-2,2]
+  a7 = [-2,-2]
+  a8 = [-2,-2,2]
+  a9 = [-2,2,2]
+  arrays = [a2, a3, a4, a5, a6, a7, a8, a9]
+
+  for a in arrays:
+    print 'Array: {0}   ||   Output: {1}'.format(a, answer(a))
+
+test_func()
+
